62878227325665794176608509827614993f5856
Arun Thampi <arun.thampi@gmail.com>
2011-12-30 22:12:31 +0800

    First commit for githeroes

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Githeroes::Application.config.secret_token = '3efe338720128a1dfce46f2c071a42a0986a2b9f1bffb5a6ddcaa54cc24ca7e63ecd4c330a15abf65b7129deb33900fe8461f5770a2d071c42b624bfbdee48d8'
