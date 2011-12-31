45cc058ab4895a17beea211d2a0a96f9ff9082d5
arunthampi <arun.thampi@gmail.com>
2011-12-31 23:26:22 +0800

    Actual hero creation

--- app/controllers/heroes_controller.rb
+++ app/controllers/heroes_controller.rb
@@ -4,7 +4,28 @@ class HeroesController < ApplicationController
   end
 
   def create
-    puts "Format: #{params[:format]}"
-    puts "Heroeeee -> #{params[:hero]}"
+    hero_params = params[:hero]
+    if (@hero = Hero.find_by_login(hero_params[:login])).blank?
+      @hero = Hero.new(
+                        :login => hero_params[:login],
+                        :email => hero_params[:email],
+                        :company => hero_params[:company],
+                        :location => hero_params[:location],
+                        :hireable => hero_params[:hireable] == 'true',
+                        :blog => hero_params[:blog],
+                        :following => hero_params[:following].to_i,
+                        :followers => hero_params[:followers].to_i,
+                        :public_repos => hero_params[:public_repos].to_i,
+                        :public_gists => hero_params[:public_gists].to_i,
+                        :bio => hero_params[:bio],
+                        :name => hero_params[:name],
+                        :avatar_url => hero_params[:avatar_url],
+                        :html_url => hero_params[:html_url],
+                        :joined_github_at => DateTime.parse(hero_params[:created_at]),
+                        :gravatar_id => hero_params[:gravatar_id],
+                        :following => hero_params[:following].to_i,
+                      )
+      @hero.save
+    end
   end
 end
--- spec/controllers/heroes_controller_spec.rb
+++ spec/controllers/heroes_controller_spec.rb
@@ -12,4 +12,97 @@ describe HeroesController do
     it { should respond_with :success }
     it { should render_template :show }
   end
+
+  describe "POST create" do
+    context "user with that Github login exists" do
+      let!(:existing_hero)  { Factory(:hero, :login => 'arunthampi') }
+
+      def do_request
+        xhr :post, :create, :hero => {
+                                        "type"=>"User",
+                                        "following"=>"18",
+                                        "blog"=>"http://posterous.mclov.in",
+                                        "login"=>"arunthampi",
+                                        "public_repos"=>"24",
+                                        "hireable"=>"false",
+                                        "bio"=>"",
+                                        "location"=>"Singapore",
+                                        "company"=>"http://anideo.com",
+                                        "email"=>"arun.thampi@gmail.com",
+                                        "avatar_url"=>"https://secure.gravatar.com/avatar/ca448f8bc4c932c6e691202a05a095a3?d=https://a248.e.akamai.net/assets.github.com%2Fimages%2Fgravatars%2Fgravatar-140.png",
+                                        "url"=>"https://api.github.com/users/arunthampi",
+                                        "gravatar_id"=>"ca448f8bc4c932c6e691202a05a095a3",
+                                        "html_url"=>"https://github.com/arunthampi",
+                                        "created_at"=>"2008-02-27T09:25:52Z",
+                                        "name"=>"Arun Thampi",
+                                        "id"=>"1048",
+                                        "public_gists"=>"39",
+                                        "followers"=>"30"
+                                     }
+      end
+
+      it "should create a new Hero" do
+        expect {
+          do_request
+        }.to change(Hero, :count).by(0)
+
+        assigns(:hero).login.should == 'arunthampi'
+      end
+
+      it "should respond with success" do
+        do_request
+        response.should be_success
+      end
+
+      it "should render template create" do
+        do_request
+        response.should render_template :create
+      end
+    end
+
+    context "user with that Github login does not exist" do
+      def do_request
+        xhr :post, :create, :hero => {
+                                        "type"=>"User",
+                                        "following"=>"18",
+                                        "blog"=>"http://posterous.mclov.in",
+                                        "login"=>"arunthampi",
+                                        "public_repos"=>"24",
+                                        "hireable"=>"false",
+                                        "bio"=>"",
+                                        "location"=>"Singapore",
+                                        "company"=>"http://anideo.com",
+                                        "email"=>"arun.thampi@gmail.com",
+                                        "avatar_url"=>"https://secure.gravatar.com/avatar/ca448f8bc4c932c6e691202a05a095a3?d=https://a248.e.akamai.net/assets.github.com%2Fimages%2Fgravatars%2Fgravatar-140.png",
+                                        "url"=>"https://api.github.com/users/arunthampi",
+                                        "gravatar_id"=>"ca448f8bc4c932c6e691202a05a095a3",
+                                        "html_url"=>"https://github.com/arunthampi",
+                                        "created_at"=>"2008-02-27T09:25:52Z",
+                                        "name"=>"Arun Thampi",
+                                        "id"=>"1048",
+                                        "public_gists"=>"39",
+                                        "followers"=>"30"
+                                     }
+      end
+
+      it "should create a new Hero" do
+        expect {
+          do_request
+        }.to change(Hero, :count).by(1)
+
+        assigns(:hero).login.should == 'arunthampi'
+      end
+
+      it "should respond with success" do
+        do_request
+        response.should be_success
+      end
+
+      it "should render template create" do
+        do_request
+        response.should render_template :create
+      end
+    end
+  end
+
 end
