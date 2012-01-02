f3e50b4ebb6ec72ee697bb0bfa1adc4cc52d6d43
arunthampi <arun.thampi@gmail.com>
2012-01-02 19:52:22 +0800

    Remove validations from the spec

--- spec/models/hero_spec.rb
+++ spec/models/hero_spec.rb
@@ -4,9 +4,7 @@ describe Hero do
   describe "Validations" do
     subject { Factory(:hero) }
 
-    it { should validate_uniqueness_of  :email }
     it { should validate_uniqueness_of  :login }
-    it { should validate_presence_of    :email }
     it { should validate_presence_of    :login }
   end
 
