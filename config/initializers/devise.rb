bdda4ca08856af08f7341dc47edcda204aa97fb3
arunthampi <arun.thampi@gmail.com>
2012-01-02 22:40:43 +0800

    Remove scope

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -196,8 +196,7 @@ Devise.setup do |config|
   # ==> OmniAuth
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
-  config.omniauth :github, Settings.github_config['client_id'], Settings.github_config['secret'],
-                           :scope => 'user,public_repo'
+  config.omniauth :github, Settings.github_config['client_id'], Settings.github_config['secret']
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
